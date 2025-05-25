2d25a3fada29f5f4d376cabfc298ed165b94572f
TJ Nicolaides <tjnicolaides@gmail.com>
2025-05-24 23:48:04 -0400

    Add Jest config

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+};
